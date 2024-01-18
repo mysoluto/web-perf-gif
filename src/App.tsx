e4597b11b31eda57543362c529ca880b46bb0e2d
xiaohai.liu <xiaohai.liu@klook.com>
2024-01-18 10:15:41 +0800

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useState } from 'react'
+import { useCallback, useState } from 'react'
 import { PageContainer } from '@ant-design/pro-components'
 import { Input, Card, message } from 'antd'
 
